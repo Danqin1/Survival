f1835271a99c746758ce939693546f624a08278f
Piotr <Piotr.dangquoc@gmail.com>
2020-10-17 21:34:41 +0200

    fixed moving smoothness

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,37 +15,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float horizontal = 0, vertical = 0;
         if (joystick.MoveThreshold > 0)
         {
-            float horizontal, vertical = 0;
-            horizontal = (joystick.Horizontal < -0.2) ? -1 : (joystick.Horizontal > 0.5f) ? 1 : joystick.Horizontal;
-            vertical = (joystick.Vertical < -0.2) ? -1 : (joystick.Vertical > 0.5f) ? 1 : joystick.Vertical;
+            horizontal = joystick.Horizontal; //(joystick.Horizontal < -0.1) ? -1 : (joystick.Horizontal > 0.1f) ? 1 : joystick.Horizontal;
+            vertical = joystick.Vertical;//(joystick.Vertical < -0.1) ? -1 : (joystick.Vertical > 0.1f) ? 1 : joystick.Vertical;
             animator.SetFloat("Y", vertical);
             animator.SetFloat("X", horizontal);
         }
-#if UNITY_EDITOR
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                animator.SetFloat("X", -1);
-            }
-            else animator.SetFloat("X", 1);
-        }
-        else animator.SetFloat("X", 0);
-
-
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.S))
-            {
-                animator.SetFloat("Y", -1);
-            }
-            else animator.SetFloat("Y", 1);
-        }
-        else animator.SetFloat("Y", 0);
-#endif
     }
 
     #endregion
